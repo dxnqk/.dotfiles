e1156be247c380e457affabdab7705300381bf9f
dxnqk <212704808+dxnqk@users.noreply.github.com>
2025-05-21 14:19:42 +0200

    Fix: Change repos dir path in tmux-session script

--- tmux/.config/tmux/tmux-session.sh
+++ tmux/.config/tmux/tmux-session.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-selected="$(find "$HOME"/repos -mindepth 1 -maxdepth 1 -type d | sort | fzf)"
+selected="$(find "$HOME/Repos" -mindepth 1 -maxdepth 1 -type d | sort | fzf)"
 [ -z "$selected" ] && exit 0
 name="$(basename "$selected" | tr "." "_")"
 tmux has-session -t "$name" 2> /dev/null || tmux new-session -ds "$name" -c "$selected"
